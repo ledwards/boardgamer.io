5781fa736d72262551862964b3d615c6b4a3d520
Lee Edwards <leeredwards@gmail.com>
2014-04-21 08:44:24 -0400

    Add games index page with the ability to create new games

--- server.go
+++ server.go
@@ -5,34 +5,63 @@ import (
   "io/ioutil"
   "github.com/martini-contrib/encoder"
   "github.com/codegangsta/martini"
+  "github.com/codegangsta/martini-contrib/binding"
+  "github.com/codegangsta/martini-contrib/render"
   "github.com/moovweb/gokogiri"
+  "labix.org/v2/mgo"
 )
 
-type DB interface {
-    Get(id int) *Game
-    Find(query string) []*Game
+type Game struct {
+  ID string           `json:"id" form:"id"`
+  MinPlayers string   `json:"minPlayers" form:"min-players"`
+  MaxPlayers string   `json:"maxPlayers" form:"max-players"`
+  Name string         `json:"name" form:"name"`
+  ImageURL string     `json:"imageURL" form:"image-url"`
+  Description string  `json:"description" form:"description"`
 }
 
-type Game struct {
-  ID string           `json:"id"`
-  MinPlayers string   `json:"minPlayers"`
-  MaxPlayers string   `json:"maxPlayers"`
-  Title string        `json:"title"`
-  ImageURL string     `json:"imageURL"`
-  Description string  `json:"description`
+func DB() martini.Handler {
+    session, err := mgo.Dial("mongodb://localhost")
+    if err != nil {
+        panic(err)
+    }
+
+    return func(c martini.Context) {
+        s := session.Clone()
+        c.Map(s.DB("boardgamer"))
+        defer s.Close()
+        c.Next()
+    }
 }
 
 func main() {
   m := martini.New()
   m.Use(martini.Recovery())
   m.Use(martini.Logger())
+  m.Use(render.Renderer())
   m.Use(JSONEncoder)
+  m.Use(DB())
 
   r := martini.NewRouter()
   r.Get("/games/:id", GetGame)
 
+  r.Get("/games", func(r render.Render, db *mgo.Database) {
+      r.HTML(200, "index", GetGames(db))
+  })
+
+  r.Post("/games", binding.Form(Game{}), func(game Game, r render.Render, db *mgo.Database) {
+      db.C("games").Insert(game)
+      r.HTML(200, "index", GetGames(db))
+  })
+
   m.Action(r.Handle)
   m.Run()
+}
+
+func GetGames(db *mgo.Database) []Game {
+    var games []Game
+    db.C("games").Find(nil).All(&games)
+    return games
 }
 
 func GetGame(enc encoder.Encoder, params martini.Params) (int, string) {
@@ -49,8 +78,8 @@ func GetGame(enc encoder.Encoder, params martini.Params) (int, string) {
     ids, _ := doc.Search("items//item")
     id := ids[0].Attribute("id").Value()
 
-    titles, _ := doc.Search("items//item//name[@type='primary']")
-    title := titles[0].Attribute("value").Value()
+    names, _ := doc.Search("items//item//name[@type='primary']")
+    name := names[0].Attribute("value").Value()
 
     descs, _ := doc.Search("items//item//description")
     desc := descs[0].Content()
@@ -64,7 +93,7 @@ func GetGame(enc encoder.Encoder, params martini.Params) (int, string) {
     images, _ := doc.Search("items//item//image")
     image := images[0].Content()
 
-    result := &Game{ Description: desc, ID: id, MinPlayers: min, MaxPlayers: max, Title: title, ImageURL: image }
+    result := &Game{ Description: desc, ID: id, MinPlayers: min, MaxPlayers: max, Name: name, ImageURL: image }
     return http.StatusOK, string(encoder.Must(enc.Encode(result)))
   }
 }
